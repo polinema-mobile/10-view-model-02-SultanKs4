f75a901b19b028bafe88f1ba579d3d0e9f2b20a7
SultanKs4 <sultangendut@gmail.com>
2020-11-05 13:15:15 +0700

    bundle error

--- app/src/main/java/id/putraprima/mvvmlogin/fragments/HomeFragment.kt
+++ app/src/main/java/id/putraprima/mvvmlogin/fragments/HomeFragment.kt
@@ -1,16 +1,24 @@
 package id.putraprima.mvvmlogin.fragments
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import id.putraprima.mvvmlogin.R
+import id.putraprima.mvvmlogin.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
+    private lateinit var binding: FragmentHomeBinding
+    lateinit var email: String
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false)
+        binding.fragment = this
+        email = "Halo, ${arguments?.getString("email").toString()}"
+        Log.e("email", email)
+        return binding.root
     }
 }
--- app/src/main/java/id/putraprima/mvvmlogin/fragments/LoginFragment.kt
+++ app/src/main/java/id/putraprima/mvvmlogin/fragments/LoginFragment.kt
@@ -1,6 +1,7 @@
 package id.putraprima.mvvmlogin.fragments
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -33,7 +34,9 @@ class LoginFragment : Fragment() {
 
 
         loginViewModel.userLiveData.observe(viewLifecycleOwner, { value ->
+            Log.e("observeUser", value.email + " " + value.password)
             mUser = value
+            Log.e("mUser", mUser.email + " " + mUser.password)
         })
 
         loginViewModel.loggedLiveData.observe(viewLifecycleOwner, { value ->
